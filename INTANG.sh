9bf7cff23b5f5a6209bdac448406cd242ad44399
Alptekin Gülcan <alptekingulcan@hotmail.com>
2019-01-21 20:51:58 +0300

    Update INTANG.sh

--- INTANG.sh
+++ INTANG.sh
@@ -36,7 +36,7 @@ iptables -X
 iptables -t raw -F 
 iptables -t raw -X
 iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
-iptables -t nat -I PREROUTING -p icmp -j DROP
+#iptables -t nat -I PREROUTING -p icmp -j DROP
 iptables -t raw -I PREROUTING -p tcp -m tcp --sport 80 --tcp-flags RST RST -j NFQUEUE --queue-num 1
 iptables -t raw -I PREROUTING -p tcp -m tcp --sport 80 --tcp-flags SYN,ACK SYN,ACK -j NFQUEUE --queue-num 1
 iptables -t mangle -I PREROUTING -p tcp -m tcp --sport 80 --tcp-flags SYN,RST,ACK ACK -m u32 --u32 "0x0>>0x16&0x3c@0xc>>0x1a&0x3c@0x0=0x48545450" -j NFQUEUE --queue-num 1
@@ -51,6 +51,9 @@ iptables -t mangle -I POSTROUTING  -p tcp -m tcp --dport 443 --tcp-flags SYN,ACK
 iptables -t mangle -I POSTROUTING  -p tcp -m tcp --dport 443 --tcp-flags SYN,RST,ACK ACK -m mark ! --mark 0x9 -m length --length 0:80 -j NFQUEUE --queue-num 1
 iptables -t mangle -I POSTROUTING  -p tcp -m tcp --dport 443 --tcp-flags SYN,RST,ACK ACK -m mark ! --mark 0x9 -m u32 --u32 "0x0>>0x16&0x3c@0xc>>0x1a&0x3c@0x0&0xffff0000=0x16030000" -j NFQUEUE --queue-num 1
       echo "Done. PID=$PID"
+      sleep 3
+iptables -F
+
    else
       echo "server is already running, PID=$PID"
    fi
